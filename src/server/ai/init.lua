ccfc52982e9a7a553559ee3923a3962a26601c90
toodols <infinitestasis@gmail.com>
2025-07-30 02:58:59 -0700

    add a* pathfind algorithm some prototype code for ai

--- src/server/ai/init.lua
+++ src/server/ai/init.lua
@@ -1,1 +1,62 @@
--- no.
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local types = require(ReplicatedStorage.Shared.types)
+local world_mod = require(ReplicatedStorage.Shared.world)
+local coords_mod = require(ReplicatedStorage.Shared.coords)
+type Interaction = types.Interaction
+type World = types.World
+type TeamData = types.TeamData
+
+type Memory = {}
+type ConfigProfile = {}
+type CandidateAction = {
+	action: Interaction,
+	score: number,
+}
+
+-- look for the closest bar deposit that is not occupied
+function claim_bar_deposit(
+	world: World,
+	team: TeamData,
+	config_profile: ConfigProfile,
+	memory: Memory
+): CandidateAction?
+	local heart = world:query_entity {
+		type = "heart",
+		owner = team.id,
+	}
+	local candidate_deposits = {}
+	for _, cell in world_mod.coords_filter(world, coords_mod.neighbors_leq(heart.primary_coordinate, 5)) do
+		for _, entity in cell.entities do
+			if entity.type == "deposit" and entity.deposit_type == "bar_deposit" then
+				table.insert(candidate_deposits, entity)
+			end
+		end
+	end
+
+	return nil
+end
+
+local actions = { claim_bar_deposit }
+
+function decide(world: World, team: TeamData, config_profile: ConfigProfile, memory: Memory)
+	local interactions = {}
+	while true do
+		local best_action = { action = nil, score = 0 }
+		for _, action in actions do
+			local candidate = action(world, memory)
+			if candidate and candidate.score > best_action.score then
+				best_action = candidate
+			end
+		end
+		if best_action.score > 0 then
+			table.insert(interactions, best_action.action)
+		else
+			break
+		end
+	end
+	return interactions
+end
+
+return {
+	decide = decide,
+}
---
+++ src/shared/world/pathfind.lua
@@ -0,0 +1,94 @@
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local types = require(ReplicatedStorage.Shared.types)
+local coords = require(ReplicatedStorage.Shared.coords)
+local util = require(ReplicatedStorage.Shared.util)
+local entity_mod = require(ReplicatedStorage.Shared.entity)
+
+local encode_coord = coords.encode_coord
+type World = types.World
+type CubicCoordinate = types.CubicCoordinate
+type EncodedCoordinate = types.EncodedCoordinate
+type TeamId = types.TeamId
+type HexCell = types.HexCell
+
+function cell_is_open(world: World, cell: HexCell?, team: TeamId): boolean
+	if not cell then
+		return false
+	end
+	for presence_team in cell.server_data.presence do
+		if presence_team ~= team then
+			return false
+		end
+	end
+	for entity_id in cell.entities do
+		local entity = world.entities[entity_id]
+		local config = world.entity_configurations[entity.type]
+		if config.layer == entity_mod.LAYER.building then
+			return false
+		end
+	end
+	return true
+end
+
+function astar(world: World, start: CubicCoordinate, goal: CubicCoordinate, team: TeamId): { CubicCoordinate }?
+	local open: { CubicCoordinate } = { start }
+	local open_set: { [EncodedCoordinate]: true } = { [encode_coord(start)] = true }
+	local from: { [EncodedCoordinate]: CubicCoordinate } = {}
+
+	local gscore_map: { [EncodedCoordinate]: number } = { [encode_coord(start)] = 0 }
+	local fscore_map: { [EncodedCoordinate]: number } = {}
+	while #open > 0 do
+		local current
+		local current_i
+		for i, v in open do
+			if
+				not current
+				or (fscore_map[encode_coord(v)] or math.huge) < (fscore_map[encode_coord(current)] or math.huge)
+			then
+				current = v
+				current_i = i
+			end
+		end
+		table.remove(open, current_i)
+		open_set[current] = nil
+		local current_key = encode_coord(current)
+
+		if coords.coords_eq(current, goal) then
+			local path = {}
+			while current do
+				table.insert(path, current)
+				current = from[encode_coord(current)]
+			end
+			return util.table_reverse(path)
+		end
+
+		for _, neighbor in coords.neighbors_eq(current, 1) do
+			local cell = world:get_cell(neighbor)
+			if not cell_is_open(world, cell, team) then
+				continue
+			end
+
+			local gscore = gscore_map[current_key] + 1
+			local neighbor_key = encode_coord(neighbor)
+			if gscore < (gscore_map[neighbor_key] or math.huge) then
+				gscore_map[neighbor_key] = gscore
+				from[neighbor_key] = current
+				fscore_map[neighbor_key] = gscore + coords.coords_dist(neighbor, goal)
+				if not open_set[neighbor_key] then
+					table.insert(open, neighbor)
+					open_set[neighbor_key] = true
+				end
+			end
+		end
+	end
+	return nil
+end
+
+function dfs(world: World, start: CubicCoordinate, team: TeamId, depth: number): { CubicCoordinate }
+	local visited = {}
+	local stack = {}
+	local depth_stack = {}
+	error "todo"
+end
+
+return { astar = astar }
